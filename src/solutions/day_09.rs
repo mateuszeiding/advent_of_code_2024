b177e6f96e63648c601bdfb00ace22dd557ad968
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-12-09 06:46:18 +0100

    Day 09 Part 01

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -2,9 +2,48 @@ use crate::utils::setup;
 
 #[allow(dead_code)]
 pub fn part_01() {
-    let input = setup::get_input_lines_vec(9, true);
+    let input = setup::get_input_lines_vec(9, false)
+        .first()
+        .unwrap()
+        .clone();
+    let disc_map: Vec<String> = input.chars().map(|x| x.to_string()).collect();
+    let mut blocks: Vec<String> = Vec::new();
 
-    println!("{:#?}", input);
+    let mut id = 0;
+    for n in 0..disc_map.len() {
+        let dig = disc_map[n].parse::<usize>().unwrap();
+        let sign: String;
+        if n % 2 == 0 {
+            let num = id.clone();
+            id += 1;
+            sign = num.to_string();
+        } else {
+            sign = ".".to_string();
+        };
+
+        for _ in 0..dig {
+            blocks.push(sign.clone());
+        }
+    }
+
+    for n in (0..blocks.len()).rev() {
+        if blocks[n] != "." {
+            let first_dot_pos = blocks.iter().position(|x| x == ".").unwrap();
+            if n > first_dot_pos {
+                blocks.swap(first_dot_pos, n);
+            }
+        }
+    }
+
+    let mut sum: usize = 0;
+    for n in 0..blocks.len() {
+        if blocks[n] == "." {
+            break;
+        }
+        sum += n * blocks[n].parse::<usize>().unwrap();
+    }
+
+    println!("{:#?}", sum);
 }
 
 #[allow(dead_code)]
@@ -13,3 +52,4 @@ pub fn part_02() {
 
     println!("{:#?}", input);
 }
+
