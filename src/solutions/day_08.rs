595cb061944fbf4415ea37c882c6aef2fa510883
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-12-09 18:27:53 +0100

    Day 08 Part 01

--- src/solutions/day_08.rs
+++ src/solutions/day_08.rs
@@ -1,15 +1,85 @@
+use crate::structs::position_struct::Position;
 use crate::utils::setup;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub fn part_01() {
-    let input = setup::get_input_lines_vec(2, false);
+    let input = setup::get_input_lines_vec(8, false);
+    let matrix = setup::get_input_matrix(input);
 
-    println!("{:#?}", input);
+    let mut antenas: HashMap<String, Vec<Position>> = HashMap::new();
+    matrix.iter().enumerate().for_each(|(i, x)| {
+        x.iter().enumerate().for_each(|(j, y)| match y.as_str() {
+            "." => {}
+            _ => {
+                antenas
+                    .entry(y.to_string())
+                    .or_insert_with(Vec::new)
+                    .push(Position { x: j, y: i });
+            }
+        });
+    });
+
+    let max_x = matrix[0].len() as i32;
+    let max_y = matrix.len() as i32;
+    let mut antinodes: Vec<Position> = Vec::new();
+    for ant in antenas.values() {
+        for n in 0..ant.len() {
+            for m in n + 1..ant.len() {
+                get_antinodes(&ant[n], &ant[m], &mut antinodes, max_x, max_y);
+            }
+        }
+    }
+
+    println!("{}", antinodes.len());
+}
+
+fn get_antinodes(
+    ant_1: &Position,
+    ant_2: &Position,
+    antinodes: &mut Vec<Position>,
+    max_x: i32,
+    max_y: i32,
+) {
+    let x_diff: i32 = ant_1.x as i32 - ant_2.x as i32;
+    let y_diff: i32 = ant_1.y as i32 - ant_2.y as i32;
+    let node_1_x;
+    let node_2_x;
+    let node_1_y;
+    let node_2_y;
+    if x_diff < 0 {
+        node_1_x = ant_2.x as i32 + x_diff.abs();
+        node_2_x = ant_1.x as i32 - x_diff.abs();
+    } else {
+        node_1_x = ant_1.x as i32 + x_diff.abs();
+        node_2_x = ant_2.x as i32 - x_diff.abs();
+    };
+    if y_diff < 0 {
+        node_1_y = ant_2.y as i32 + y_diff.abs();
+        node_2_y = ant_1.y as i32 - y_diff.abs();
+    } else {
+        node_1_y = ant_1.y as i32 + y_diff.abs();
+        node_2_y = ant_2.y as i32 - y_diff.abs();
+    }
+
+    if node_1_y >= 0 && node_1_y < max_y && node_1_x >= 0 && node_1_x < max_x {
+        antinodes.push(Position {
+            x: node_1_x as usize,
+            y: node_1_y as usize,
+        });
+    }
+
+    if node_1_y >= 0 && node_1_y < max_y && node_1_x >= 0 && node_1_x < max_x {
+        antinodes.push(Position {
+            x: node_2_x as usize,
+            y: node_2_y as usize,
+        });
+    }
 }
 
 #[allow(dead_code)]
 pub fn part_02() {
-    let input = setup::get_input_lines_vec(2, false);
+    let input = setup::get_input_lines_vec(8, false);
 
     println!("{:#?}", input);
 }
--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -34,7 +34,7 @@ pub fn part_01() {
             }
         }
     }
-
+    println!("{:?}", blocks);
     let mut sum: usize = 0;
     for n in 0..blocks.len() {
         if blocks[n] == "." {
@@ -43,13 +43,84 @@ pub fn part_01() {
         sum += n * blocks[n].parse::<usize>().unwrap();
     }
 
-    println!("{:#?}", sum);
+    println!("{} {}", sum, blocks.len());
 }
 
 #[allow(dead_code)]
 pub fn part_02() {
-    let input = setup::get_input_lines_vec(9, false);
+    let input = setup::get_input_lines_vec(9, false)
+        .first()
+        .unwrap()
+        .clone();
+    let disc_map: Vec<String> = input.chars().map(|x| x.to_string()).collect();
+    let mut blocks: Vec<String> = Vec::new();
 
-    println!("{:#?}", input);
+    let mut id = 0;
+    for n in 0..disc_map.len() {
+        let dig = disc_map[n].parse::<usize>().unwrap();
+        let sign: String;
+        if n % 2 == 0 {
+            let num = id.clone();
+            id += 1;
+            sign = num.to_string();
+        } else {
+            sign = ".".to_string();
+        };
+
+        let mut new_val: String = String::new();
+        for _ in 0..dig {
+            new_val += &(",".to_string() + &sign);
+        }
+        if new_val.len() != 0 {
+            blocks.push(new_val);
+        }
+    }
+
+    for n in (0..blocks.len()).rev() {
+        if !blocks[n].contains(".") {
+            let first_dot_pos = blocks.iter().position(|x| {
+                x.contains(".") && x.replace(",", "").len() >= blocks[n].replace(",", "").len()
+            });
+
+            match first_dot_pos {
+                Some(dfp) => {
+                    if n > dfp {
+                        let diff =
+                            blocks[dfp].replace(",", "").len() - blocks[n].replace(",", "").len();
+                        blocks.swap(dfp, n);
+                        if diff > 0 {
+                            blocks[n] = blocks[n][0..blocks[n].len() - diff].to_string();
+                            blocks.insert(dfp + 1, ".".to_string().repeat(diff));
+                        }
+                    }
+                }
+                None => continue,
+            }
+        }
+    }
+
+    let mut sum: usize = 0;
+    let mut iter: usize = 0;
+    for n in 0..blocks.len() {
+        if blocks[n].contains(".") {
+            let wo_comma: Vec<_> = blocks[n]
+                .chars()
+                .filter(|x| x.to_string() == ".".to_string())
+                .collect();
+            iter += wo_comma.len();
+            continue;
+        }
+        let chars: Vec<usize> = blocks[n]
+            .split(",")
+            .filter(|x| !x.is_empty())
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        println!("{:?}", chars);
+        for m in 0..chars.len() {
+            sum += iter * chars[m];
+            iter += 1;
+        }
+    }
+
+    println!("{} {}", sum, iter);
 }
-
