13b9b662713535e750dacb0a31400affeb482b32
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-12-11 21:18:45 +0100

    What I can do? 2024?

--- src/solutions/day_11.rs
+++ src/solutions/day_11.rs
@@ -1,5 +1,6 @@
 use crate::utils::setup;
 use std::time::Instant;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub fn part_01() {
@@ -25,15 +26,24 @@ pub fn part_01() {
 
 fn blink(stones: &mut Vec<usize>) {
     let mut new_stones: Vec<usize> = Vec::new();
-    stones.iter().for_each(|stone| match stone {
-        s if s == &0 => new_stones.push(1),
-        s if s.to_string().len() % 2 == 0 => {
-            let dig = (*s as f64).log(10.0).floor() as usize + 1;
-            let div = 10_usize.pow((dig / 2) as u32);
-            new_stones.push(s / div);
-            new_stones.push(s & div);
-        }
-        s => new_stones.push(s * 2024),
+    stones.iter().for_each(|&stone|  {
+        if stone == 0 {
+            new_stones.push(1);
+            return;
+        };
+
+        let dig = (stone as f64).log(10.0).floor() as usize + 1;
+        match dig % 2 {
+            0 =>  {
+                let div = 10_usize.pow((dig / 2) as u32);
+                new_stones.push(stone / div);
+                new_stones.push(stone % div);
+            },
+            1 => {
+                new_stones.push((stone << 10) +  (stone << 9) + (stone << 8) +(stone << 7) +(stone << 6) + (stone << 5) + (stone << 3))
+            }
+            _ => {}
+        };
     });
 
     *stones = new_stones;
@@ -53,11 +63,12 @@ pub fn part_02() {
 
     let blinker_guy = 75;
 
-    for n in 0..blinker_guy {
+    for n in 0..blinker_guy  {
         let start = Instant::now();
         blink(&mut stones);
         let dur = start.elapsed();
         println!("{} {:?}", n, dur);
     }
+
     println!("---{}", stones.len());
 }
