843dd1ff0bc7a1a56e164284adb65d5ee6a2a17e
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-12-11 07:14:04 +0100

    Day 11 Part 02 - Opt 01 ~ x1527

--- src/solutions/day_11.rs
+++ src/solutions/day_11.rs
@@ -1,4 +1,6 @@
 use crate::utils::setup;
+use std::collections::HashMap;
+use std::time::Instant;
 
 #[allow(dead_code)]
 pub fn part_01() {
@@ -23,30 +25,39 @@ pub fn part_01() {
 }
 
 fn blink(stones: &mut Vec<usize>) {
-    let mut ctr = stones.len();
-    let mut i = 0;
-    while i < ctr {
-        match stones[i] {
-            s if s == 0 => stones[i] = 1,
-            s if s.to_string().len() % 2 == 0 => {
-                let stn = s.to_string();
-                stones[i] = stn[0..stn.len() / 2].parse::<usize>().unwrap();
-                stones.insert(
-                    i + 1,
-                    stn[stn.len() / 2..stn.len()].parse::<usize>().unwrap(),
-                );
-                ctr += 1;
-                i += 1;
-            }
-            s => stones[i] = s * 2024,
+    let mut new_stones: Vec<usize> = Vec::new();
+    stones.iter().for_each(|stone| match stone {
+        s if s == &0 => new_stones.push(1),
+        s if s.to_string().len() % 2 == 0 => {
+            let stn = s.to_string();
+            new_stones.push(stn[0..stn.len() / 2].parse::<usize>().unwrap());
+            new_stones.push(stn[stn.len() / 2..stn.len()].parse::<usize>().unwrap());
         }
-        i += 1;
-    }
+        s => new_stones.push(s * 2024),
+    });
+
+    *stones = new_stones;
 }
 
 #[allow(dead_code)]
 pub fn part_02() {
-    let input = setup::get_input_lines_vec(2, false);
+    let input = setup::get_input_lines_vec(11, false);
 
-    println!("{:#?}", input);
+    let mut stones: Vec<usize> = input
+        .first()
+        .unwrap()
+        .split(" ")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+
+    let blinker_guy = 75;
+
+    for n in 0..blinker_guy {
+        let start = Instant::now();
+        blink(&mut stones);
+        let dur = start.elapsed();
+        println!("{} {:?}", n, dur);
+    }
+    println!("---{}", stones.len());
 }
